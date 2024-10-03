17d373f9cbdaddb7e01920bcc509ccf6cc7afaf8
MudSlingar <muddersling@gmail.com>
2024-10-03 15:21:20 +0200

    Quartz sync: Oct 3, 2024, 3:21 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161610",
+          light: "#614640",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
