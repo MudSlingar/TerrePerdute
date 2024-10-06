e5bec35eb3756fc47fb3309d0dc3a78ce9389a6f
MudSlingar <muddersling@gmail.com>
2024-10-06 18:15:32 +0200

    Quartz sync: Oct 6, 2024, 6:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,7 +21,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       fontOrigin: "local",
-      cdnCaching: false,
+      cdnCaching: true,
       typography: {
          header: "HamletOrNot",  //"Pangolin",
          body: "Kelvinch",       //"Crimson Text",
