e79afc61f6253244fb81d4a73758ad7a09c23eb4
MudSlingar <muddersling@gmail.com>
2025-03-28 23:05:48 +0100

    Quartz sync: Mar 28, 2025, 11:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
-      //fontOrigin: "local",
+      fontOrigin: "Google Fonts",
       cdnCaching: true,
       typography: {
          header: "Pangolin",
